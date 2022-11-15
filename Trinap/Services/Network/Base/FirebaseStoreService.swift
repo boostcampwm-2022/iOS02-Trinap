ead7537e7e8ea13207b9c3fb5e5546d8ed4e14c5
d0yvn <doyun0887@gmail.com>
2022-11-15 17:24:42 +0900

    feat: FireStoreService 구현 - #108
    resolved #108

---
+++ Trinap/Services/Network/Base/FirebaseStoreService.swift
@@ -0,0 +1,23 @@
+//
+//  FirebaseStoreService.swift
+//  Trinap
+//
+//  Created by Doyun Park on 2022/11/15.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol FirebaseStoreService {
+    
+    associatedtype FirebaseData
+    
+    func getDocument(collection: String) -> Single<[FirebaseData]>
+    func getDocument(collection: String, document: String) -> Single<FirebaseData>
+    func getDocument(collection: String, field: String, condition: [String]) -> Single<[FirebaseData]>
+    func createDocument(collection: String, document: String, values: FirebaseData) -> Single<Void>
+    func updateDocument(collection: String, document: String, values: FirebaseData) -> Single<Void>
+    func deleteDocument(collection: String, document: String, values: FirebaseData) -> Single<Void>
+    func observer(collection: String, document: String) -> Observable<FirebaseData>
+}
