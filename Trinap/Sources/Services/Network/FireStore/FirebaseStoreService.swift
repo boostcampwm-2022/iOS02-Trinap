a43dae29820e967e24afdfd2db5685b88ec2fd71
chansooo <kcs05008@gmail.com>
2022-11-15 21:10:17 +0900

    feat: `FirebaseStoreService` 프로토콜에 document로 접근하는 메소드 구현 - #108

--- Trinap/Sources/Services/Network/FireStore/FirebaseStoreService.swift
+++ Trinap/Sources/Services/Network/FireStore/FirebaseStoreService.swift
@@ -16,8 +16,12 @@ protocol FirebaseStoreService {
     func getDocument(collection: String) -> Single<[FirebaseData]>
     func getDocument(collection: String, document: String) -> Single<FirebaseData>
     func getDocument(collection: String, field: String, condition: [String]) -> Single<[FirebaseData]>
+    func getDocument(documents: [String]) -> Single<FirebaseData>
     func createDocument(collection: String, document: String, values: FirebaseData) -> Single<Void>
+    func createDocument(documents: [String], values: FirebaseData) -> Single<Void>
+
     func updateDocument(collection: String, document: String, values: FirebaseData) -> Single<Void>
     func deleteDocument(collection: String, document: String, values: FirebaseData) -> Single<Void>
     func observer(collection: String, document: String) -> Observable<FirebaseData>
+    func observer(documents: [String]) -> Observable<FirebaseData>
 }
