c85bd6ff3d67b2d3e9e4b76f5c24b4c3316e9823
chansooo <kcs05008@gmail.com>
2022-11-16 21:54:28 +0900

    feat: firestore `where( in: )` 사용한 메소드 추가

--- Trinap/Sources/Services/Network/FireStore/FirebaseStoreService.swift
+++ Trinap/Sources/Services/Network/FireStore/FirebaseStoreService.swift
@@ -18,6 +18,7 @@ protocol FireStoreService {
     func getDocument(collection: FireStoreCollection) -> Single<[FirebaseData]>
     func getDocument(collection: FireStoreCollection, document: String) -> Single<FirebaseData>
     func getDocument(collection: FireStoreCollection, field: String, condition: [String]) -> Single<[FirebaseData]>
+    func getDocument(collection: String, field: String, in values: [Any]) -> Single<[FirebaseData]>
     func getDocument(documents: [String]) -> Single<FirebaseData>
     func createDocument(collection: FireStoreCollection, document: String, values: FirebaseData) -> Single<Void>
     func createDocument(documents: [String], values: FirebaseData) -> Single<Void>
