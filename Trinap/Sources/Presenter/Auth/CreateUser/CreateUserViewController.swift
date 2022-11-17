72dd03c47880be1556ccc068acb80539b44dccd3
byeongjooYoo <yoobj17@gmail.com>
2022-11-18 02:49:46 +0900

    add: Test를 위한 CreateUserViewController 추가

---
+++ Trinap/Sources/Presenter/Auth/CreateUser/CreateUserViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CreateUserViewController.swift
+//  Trinap
+//
+//  Created by ByeongJu Yu on 2022/11/17.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class CreateUserViewController: BaseViewController {
+    
+//    // MARK: - UI
+//
+//    // MARK: - Properties
+//    private let viewModel: CreateUserViewModel
+//
+//    // MARK: - Initializers
+//    init(viewModel: CreateUserViewModel) {
+//        self.viewModel = viewModel
+//
+//        super.init()
+//    }
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    
+//    override func configureHierarchy() {
+//        [
+//            <#view#>,
+//        ].forEach { self.view.addSubview($0) }
+//    }
+//
+//    override func configureConstraints() {
+//        <#view#>.snp.makeConstrains { make in
+//            make.<#code#>
+//        }
+//    }
+    
+//    override func configureAttributes() {
+//        <#code#>
+//    }
+//
+//    override func bind() {
+//        <#code#>
+//    }
+}
---
+++ Trinap/Sources/Presenter/Auth/CreateUser/CreateUserViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  CreateUserViewModel.swift
+//  Trinap
+//
+//  Created by ByeongJu Yu on 2022/11/17.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import RxCocoa
+import RxRelay
+import RxSwift
+
+//final class CreateUserViewModel: ViewModelType {
+//
+//    struct Input {
+//        <#code#>
+//    }
+//
+//    struct Output {
+//        <#code#>
+//    }
+//
+//    // MARK: - Properties
+//    let disposeBag = DisposeBag()
+//
+//    private let <#useCase#>: <#UseCaseType#>
+//
+//    // MARK: - Initializer
+//    init(<#parameters#>) {
+//        <#statements#>
+//    }
+//
+//    // MARK: - Methods
+//    func transform(input: Input) -> Output {
+//
+//        return Output(<#code#>)
+//    }
+//}
