8509dc956d1b153146f5f1ca526bc44a3a0b38f8
byeongjooYoo <yoobj17@gmail.com>
2022-11-19 22:37:40 +0900

    refactor: Tokenmanager에 KeychainAccount을 String -> Enum으로 수정 #147

--- Trinap/Sources/Utils/TokenManager/TokenManager.swift
+++ Trinap/Sources/Utils/TokenManager/TokenManager.swift
@@ -12,17 +12,17 @@ typealias Token = String
 
 protocol TokenManager {
     
-    /// `TokenManager`에 저장되어 있는 토큰을 반환합니다.
+    /// `TokenManager`에 `key`로 저장되어 있는 토큰을 반환합니다.
     /// - Returns: 토큰이 저장되어 있으면 토큰을, 없으면 `nil`을 반환합니다.
-    func getToken() -> Token?
+    func getToken(with key: KeychainAccount) -> Token?
     
-    /// `TokenManager`에 토큰을 저장 및 업데이트합니다.
+    /// `TokenManager`에 `key`에 해당하는 토큰을 저장 및 업데이트합니다.
     /// - Returns: 토큰 저장에 성공하면 `true`를, 실패하면 `false`를 반환합니다.
     @discardableResult
-    func save(token: Token) -> Bool
+    func save(token: Token, with key: KeychainAccount) -> Bool
     
-    /// `TokenManager`에서 토큰을 삭제합니다.
+    /// `TokenManager`에서 `key`에 해당하는 토큰을 삭제합니다.
     /// - Returns: 토큰이 존재하지 않거나 삭제에 성공한 경우 `true`를, 실패하면 `false`를 반환합니다.
     @discardableResult
-    func deleteToken() -> Bool
+    func deleteToken(with key: KeychainAccount) -> Bool
 }
