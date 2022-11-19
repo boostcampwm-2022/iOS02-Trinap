73c466b38f24640d3b1566141190b42c1f9a3e7a
Seyoung Kim <yy0867@gmail.com>
2022-11-19 12:29:54 +0900

    feat: KeyboardObserving 구현

---
+++ Trinap/Sources/Presenter/Common/Extension/UIViewController+Keyboard.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController+Keyboard.swift
+//  Trinap
+//
+//  Created by 김세영 on 2022/11/19.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+
+extension UIViewController {
+    
+    /// 키보드가 나타날 때 `CGRect`를 방출합니다.
+    /// `CGRect`는 키보드의 크기를 나타냅니다.
+    var keyboardWillShowObserver: Signal<CGRect> {
+        return NotificationCenter.default.rx
+            .notification(UIResponder.keyboardWillShowNotification)
+            .map(\.userInfo)
+            .compactMap { ($0?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue }
+            .asSignal(onErrorJustReturn: .zero)
+    }
+    
+    /// 키보드가 숨겨질 때 `Void` 이벤트를 방출합니다.
+    var keyboardWillHideObserver: Signal<Void> {
+        return NotificationCenter.default.rx
+            .notification(UIResponder.keyboardWillHideNotification)
+            .map { _ in return }
+            .asSignal(onErrorJustReturn: ())
+    }
+}
