78c6bf784aed30c142da76ed01bf3e668b94cda9
chansooo <kcs05008@gmail.com>
2022-11-22 01:25:45 +0900

    feat: `DefaultMapService` 검색어 넣는 메소드 구현

---
+++ Trinap/Sources/Service/DefaultMapService.swift
@@ -0,0 +1,43 @@
+//
+//  DefaultMapService.swift
+//  Trinap
+//
+//  Created by kimchansoo on 2022/11/21.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import MapKit
+import Foundation
+
+import RxSwift
+import RxRelay
+
+final class DefaultMapService: NSObject, MapService {
+
+    // MARK: Properties
+    private let disposebag = DisposeBag()
+    private var searchCompleter = MKLocalSearchCompleter()
+    private let locationManager = CLLocationManager()
+    
+    var results = BehaviorRelay<[Space]>(value: [])
+    var curCoordinate = PublishRelay<Coordinate>()
+    
+    // MARK: Initializers
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    // MARK: Methods
+    func setSearchText(with searchText: String) {
+        searchCompleter.queryFragment = searchText
+    }
+
+}
+
