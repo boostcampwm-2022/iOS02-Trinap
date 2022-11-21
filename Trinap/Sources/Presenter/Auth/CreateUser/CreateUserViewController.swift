05af1fad57e61e6034146eb88cf52e6d03d1a56b
byeongjooYoo <yoobj17@gmail.com>
2022-11-21 21:23:57 +0900

    style: CreateUserViewController, ViewModel 임시 구현

--- Trinap/Sources/Presenter/Auth/CreateUser/CreateUserViewController.swift
+++ Trinap/Sources/Presenter/Auth/CreateUser/CreateUserViewController.swift
@@ -11,44 +11,115 @@ import UIKit
 import RxCocoa
 import RxSwift
 import SnapKit
+import Than
 
 final class CreateUserViewController: BaseViewController {
     
-//    // MARK: - UI
-//
-//    // MARK: - Properties
-//    private let viewModel: CreateUserViewModel
-//
-//    // MARK: - Initializers
-//    init(viewModel: CreateUserViewModel) {
-//        self.viewModel = viewModel
-//
-//        super.init()
-//    }
+    // MARK: - UI
+    private lazy var titleLabel = UILabel().than {
+        $0.text = "만나서 반가워요!\nTrinap과 추억을 만들어 보세요"
+        $0.numberOfLines = 0
+        $0.font = TrinapFontFamily.Pretendard.bold.font(size: 24)
+    }
+    
+    private lazy var subTitleLabel = UILabel().than {
+        $0.text = "Trinap 이용을 위해 닉네임을 설정해 주세요."
+        $0.font = TrinapFontFamily.Pretendard.regular.font(size: 14)
+    }
+    
+    private lazy var nicknameTextFieldView = NicknameTextFieldView()
+    
+    private lazy var signUpButton = TrinapButton(
+        style: .primary
+    ).than {
+        $0.setTitle("가입 완료", for: .normal)
+        $0.setTitleColor(TrinapAsset.white.color, for: .normal)
+        $0.titleLabel?.font = TrinapFontFamily.Pretendard.bold.font(size: 16)
+        $0.isEnabled = false
+    }
+
+    // MARK: - Properties
+    private let viewModel: CreateUserViewModel
+
+    // MARK: - Initializers
+    init(viewModel: CreateUserViewModel) {
+        self.viewModel = viewModel
+
+        super.init()
+    }
     
     // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
     }
     
-//    override func configureHierarchy() {
-//        [
-//            <#view#>,
-//        ].forEach { self.view.addSubview($0) }
-//    }
-//
-//    override func configureConstraints() {
-//        <#view#>.snp.makeConstrains { make in
-//            make.<#code#>
-//        }
-//    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
     
-//    override func configureAttributes() {
-//        <#code#>
-//    }
-//
-//    override func bind() {
-//        <#code#>
-//    }
+    override func configureHierarchy() {
+        [
+            titleLabel,
+            subTitleLabel,
+            nicknameTextFieldView,
+            signUpButton
+        ].forEach { self.view.addSubview($0) }
+    }
+
+    override func configureConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            make.centerX.equalToSuperview()
+            make.leading.equalToSuperview().offset(16)
+        }
+                
+        subTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.leading.centerX.equalTo(titleLabel)
+        }
+
+        nicknameTextFieldView.snp.makeConstraints { make in
+            make.top.equalTo(subTitleLabel.snp.bottom).offset(24)
+            make.leading.centerX.equalTo(titleLabel)
+            make.height.equalTo(48)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextFieldView.snp.bottom).offset(16)
+            make.leading.centerX.equalTo(titleLabel)
+            make.height.equalTo(48)
+        }
+    }
+    
+    override func configureAttributes() {
+        configureNavigationBar()
+    }
+
+    override func bind() {
+        let input = CreateUserViewModel.Input(
+            nickname: nicknameTextFieldView.textField.rx.text.orEmpty.asObservable(),
+            signUpButtonTap: signUpButton.rx.tap.asObservable())
+        
+        let output = viewModel.transform(input: input)
+        
+        output.signUpButtonEnable
+            .drive { [weak self] result in
+                self?.signUpButton.isEnabled = result
+            }
+            .disposed(by: disposeBag)
+        
+        output.signUpFailure
+            .asObservable()
+            .subscribe {
+                Logger.print("회원가입 실패")
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func configureNavigationBar() {
+        self.navigationController?.navigationBar.backgroundColor = TrinapAsset.white.color
+        self.navigationController?.navigationBar.barTintColor = TrinapAsset.white.color
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
 }
--- Trinap/Sources/Presenter/Auth/CreateUser/CreateUserViewModel.swift
+++ Trinap/Sources/Presenter/Auth/CreateUser/CreateUserViewModel.swift
@@ -10,29 +10,60 @@ import RxCocoa
 import RxRelay
 import RxSwift
 
-//final class CreateUserViewModel: ViewModelType {
-//
-//    struct Input {
-//        <#code#>
-//    }
-//
-//    struct Output {
-//        <#code#>
-//    }
-//
-//    // MARK: - Properties
-//    let disposeBag = DisposeBag()
-//
-//    private let <#useCase#>: <#UseCaseType#>
-//
-//    // MARK: - Initializer
-//    init(<#parameters#>) {
-//        <#statements#>
-//    }
-//
-//    // MARK: - Methods
-//    func transform(input: Input) -> Output {
-//
-//        return Output(<#code#>)
-//    }
-//}
+final class CreateUserViewModel: ViewModelType {
+    
+    struct Input {
+        let nickname: Observable<String>
+        let signUpButtonTap: Observable<Void>
+    }
+    
+    struct Output {
+        let signUpButtonEnable: Driver<Bool>
+        let signUpFailure: Signal<Void>
+    }
+    
+    // MARK: - Properties
+    weak var coordinator: AuthCoordinator?
+    private let createUserUseCase: CreateUserUseCase
+    private let signUpSuccess = PublishRelay<Void>()
+    private let signUpFailure = PublishRelay<Void>()
+    let disposeBag = DisposeBag()
+    
+    
+    // MARK: - Initializer
+    init(
+        createUserUseCase: CreateUserUseCase,
+        coordinator: AuthCoordinator
+    ) {
+        self.createUserUseCase = createUserUseCase
+        self.coordinator = coordinator
+    }
+    
+    // MARK: - Methods
+    func transform(input: Input) -> Output {
+        input.signUpButtonTap
+            .withLatestFrom(input.nickname)
+            .withUnretained(self)
+            .flatMap { owner, nickname in
+                return owner.createUserUseCase.createUser(with: nickname)
+            }
+            .subscribe { [weak self] in
+                Logger.print("유저 생성 성공!")
+                self?.coordinator?.finish()
+            }
+            .disposed(by: disposeBag)
+        
+        let signUpButtonEnable = input.nickname
+            .map { nickname in
+                return !nickname.isEmpty ? true : false
+            }
+            .distinctUntilChanged()
+            .asDriver(onErrorJustReturn: false)
+        
+        
+        return Output(
+            signUpButtonEnable: signUpButtonEnable,
+            signUpFailure: signUpFailure.asSignal()
+        )
+    }
+}
