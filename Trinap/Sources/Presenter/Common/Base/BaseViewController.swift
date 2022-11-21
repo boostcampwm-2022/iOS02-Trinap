5c1cd4d5ad289f432234ad73cc8657d994e935c9
d0yvn <doyun0887@gmail.com>
2022-11-21 10:49:45 +0900

    fix: BackgroundColor 잘못된 설정 추가

--- Trinap/Sources/Presenter/Common/Base/BaseViewController.swift
+++ Trinap/Sources/Presenter/Common/Base/BaseViewController.swift
@@ -18,7 +18,7 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = TrinapAsset.white.color
         
         configureAttributes()
         configureHierarchy()
