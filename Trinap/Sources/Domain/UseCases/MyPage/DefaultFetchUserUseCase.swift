83d03b7d00446f7fccdbd124d12a43b6c7add70a
d0yvn <doyun0887@gmail.com>
2022-11-21 19:22:41 +0900

    feat: FetchUserUseCase 구현 - #77

---
+++ Trinap/Sources/Domain/UseCases/MyPage/DefaultFetchUserUseCase.swift
@@ -0,0 +1,35 @@
+//
+//  DefaultFetchUserUseCase.swift
+//  Trinap
+//
+//  Created by Doyun Park on 2022/11/21.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+final class DefaultFetchUserUseCase: FetchUserUseCase {
+    
+    private let userRepository: UserRepository
+    private let tokenManager: TokenManager
+    
+    init(
+        userRepository: UserRepository,
+        tokenManager: TokenManager = KeychainTokenManager()
+    ) {
+        self.userRepository = userRepository
+        self.tokenManager = tokenManager
+    }
+    
+    func fetchUserInfo() -> Observable<User> {
+        tokenManager.save(token: "RbsMGyMhufNr4wqF2blOzvL9y092", with: .userId)
+        
+        guard let token = tokenManager.getToken(with: .userId) else {
+            return .error(TokenManagerError.notFound)
+        }
+        
+        return userRepository.fetch(userId: token)
+    }
+}
