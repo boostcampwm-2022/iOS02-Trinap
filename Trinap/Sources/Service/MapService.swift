3a7665a378e2b48b2c50876591a4716824ec7074
chansooo <kcs05008@gmail.com>
2022-11-22 01:24:32 +0900

    feat: 지도, 검색 관련 역할 하는 `MapService` 프로토콜 정의

---
+++ Trinap/Sources/Service/MapService.swift
@@ -0,0 +1,21 @@
+//
+//  MapService.swift
+//  Trinap
+//
+//  Created by kimchansoo on 2022/11/21.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import MapKit
+import Foundation
+
+import RxSwift
+
+protocol MapService {
+    // MARK: Properties
+    var results: BehaviorRelay<[Space]> { get }
+    
+    // MARK: Methods
+    func setSearchText(with searchText: String)
+    func fetchCurrentLocation() -> Observable<Coordinate>
+}
