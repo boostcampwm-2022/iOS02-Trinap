a0fdf908ba9ff63cf22e84feef9717a3530bc0d4
byeongjooYoo <yoobj17@gmail.com>
2022-11-22 15:13:11 +0900

    style: SplashViewController, SplashViewModel 구현

---
+++ Trinap/Sources/App/Splash/SplashViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SplashViewController.swift
+//  Trinap
+//
+//  Created by 김세영 on 2022/11/15.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import UIKit
+
+final class SplashViewController: BaseViewController {
+    
+    // MARK: - UI
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = TrinapAsset.logoVertical.image
+        return imageView
+    }()
+    
+    // MARK: - Properties
+    private let viewModel: SplashViewModel
+    
+    // MARK: - Properties
+    init(viewModel: SplashViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.autoSignInFlow()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(logoImageView)
+    }
+    
+    override func configureConstraints() {
+        logoImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().multipliedBy(0.8)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(50)
+        }
+    }
+}
