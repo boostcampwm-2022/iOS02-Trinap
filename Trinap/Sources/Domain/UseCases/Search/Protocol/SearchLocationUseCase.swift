0a09e06dbecd6662ab46e16866f732234e5441b5
chansooo <kcs05008@gmail.com>
2022-11-22 16:32:59 +0900

    feat: `SearchLocationUseCase` 프로토콜 정의

---
+++ Trinap/Sources/Domain/UseCases/Search/Protocol/SearchLocationUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  SearchLocationUseCase.swift
+//  Trinap
+//
+//  Created by kimchansoo on 2022/11/21.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import MapKit
+import Foundation
+
+import RxSwift
+import RxRelay
+
+protocol SearchLocationUseCase {
+    // MARK: Methods
+    func fetchSearchList(with searchText: String) -> Observable<[Space]>
+}
