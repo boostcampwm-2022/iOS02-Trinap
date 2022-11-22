0ebaccd5c9aa7c38c8e117c32d80a3bfcd59ab0e
d0yvn <doyun0887@gmail.com>
2022-11-23 01:27:21 +0900

    feat: EditUserUseCase 구현#79

---
+++ Trinap/Sources/Domain/UseCases/MyPage/DefaultEditUserUseCase.swift
@@ -0,0 +1,28 @@
+//
+//  DefaultEditUserUseCase.swift
+//  Trinap
+//
+//  Created by Doyun Park on 2022/11/22.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+final class DefaultEditUserUseCase: EditUserUseCase {
+    
+    private let userRepository: UserRepository
+    
+    init(userRepository: UserRepository) {
+        self.userRepository = userRepository
+    }
+
+    func updateProfielInfo(profileImage: String?, nickName: String?) -> Observable<Void> {
+        return userRepository.update(profileImage: profileImage, nickname: nickName, isPhotographer: nil)
+    }
+    
+    func fetchRandomNickName() -> Observable<String> {
+        return userRepository.createRandomNickname()
+    }
+}
---
+++ Trinap/Sources/Domain/UseCases/MyPage/EditUserUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  EditUserUseCase.swift
+//  Trinap
+//
+//  Created by Doyun Park on 2022/11/22.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol EditUserUseCase {
+    func updateProfielInfo(profileImage: String?, nickName: String?) -> Observable<Void>
+    func fetchRandomNickName() -> Observable<String>
+}
