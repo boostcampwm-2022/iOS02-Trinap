446df4ddeed04aafa496ba029fb6737c77161fdc
chansooo <kcs05008@gmail.com>
2022-11-24 22:25:07 +0900

    feat: `PhotographerListViewModel` 값 바인딩 처리

--- Trinap/Sources/Presenter/TabBar/Main/PhotographerList/PhotographerListViewModel.swift
+++ Trinap/Sources/Presenter/TabBar/Main/PhotographerList/PhotographerListViewModel.swift
@@ -13,6 +13,9 @@ import RxSwift
 final class PhotographerListViewModel: ViewModelType {
     
     struct Input {
+        // searchTrigger -> 화면 전환
+        // 검색어 다 치고 다시 돌어왔을 때 값 받아서 검색어에 저장 <- 얘를 어떻게 해야하지..?
+        // 필터 고를 때마다 필터 값 저장
         let searchTrigger: Observable<Void>
         var coordinate: Observable<Coordinate?>
         var tagType: Observable<TagType>
@@ -25,18 +28,39 @@ final class PhotographerListViewModel: ViewModelType {
 
     // MARK: - Properties
     let disposeBag = DisposeBag()
-    let searchText = BehaviorRelay<String>(value: "")
-    
-    private let photographersUseCase: FetchPhotographersUseCase
-    
+    private weak var coordinator: MainCoordinator?
+    private let previewsUseCase: FetchPhotographerPreviewsUseCase
+
     // MARK: - Initializer
-    init(photographersUseCase: FetchPhotographersUseCase) {
-        self.photographersUseCase = photographersUseCase
+    init(
+        previewsUseCase: FetchPhotographerPreviewsUseCase,
+        coordinator: MainCoordinator
+    ) {
+        self.previewsUseCase = previewsUseCase
+        self.coordinator = coordinator
     }
-    
+
     // MARK: - Methods
     func transform(input: Input) -> Output {
         
-        return Output(<#code#>)
+        input.searchTrigger
+            .subscribe(onNext: { [weak self] _ in
+                self?.coordinator?.showSearchViewController(searchText: input.searchText)
+            })
+            .disposed(by: disposeBag)
+        
+        let previews = Observable.combineLatest(
+            input.coordinate,
+            input.tagType
+                .debounce(.seconds(1), scheduler: MainScheduler.instance)
+        )
+            .withUnretained(self)
+            .flatMap { (owner, val) -> Observable<[PhotographerPreview]> in
+                let (coordinate, type) = val
+                return owner.previewsUseCase.fetch(coordinate: coordinate, type: type)
+            }
+            .asDriver(onErrorJustReturn: [])
+        
+        return Output(previews: previews)
     }
 }
