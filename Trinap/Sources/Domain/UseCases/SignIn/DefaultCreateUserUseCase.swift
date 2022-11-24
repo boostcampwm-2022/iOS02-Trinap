2d6af31457036154f9336a8203dd133f383ac50c
byeongjooYoo <yoobj17@gmail.com>
2022-11-24 20:10:27 +0900

    feat: User collection 생성 시 photographer collection도 함께 생성하도록 수정

--- Trinap/Sources/Domain/UseCases/SignIn/DefaultCreateUserUseCase.swift
+++ Trinap/Sources/Domain/UseCases/SignIn/DefaultCreateUserUseCase.swift
@@ -15,16 +15,39 @@ final class DefaultCreateUserUseCase: CreateUserUseCase {
     // MARK: - Properties
     private let authRepository: AuthRepository
     private let userRepository: UserRepository
+    private let photographerRepository: PhotographerRepository
     
     // MARK: - Initializers
-    init(authRepository: AuthRepository, userRepository: UserRepository) {
+    init(
+        authRepository: AuthRepository,
+        userRepository: UserRepository,
+        photographerRepository: PhotographerRepository
+    ) {
         self.authRepository = authRepository
         self.userRepository = userRepository
+        self.photographerRepository = photographerRepository
     }
     
     // MARK: - Methods
     func createUser(with nickName: String) -> Observable<Void> {
         return self.authRepository.createUser(nickname: nickName)
+            .withUnretained(self)
+            .flatMap { owner, _ in
+                owner.photographerRepository.create(
+                    photographer: Photographer(
+                        photographerId: UUID().uuidString,
+                        photographerUserId: "",
+                        introduction: "",
+                        latitude: 37.7,
+                        longitude: 127,
+                        tags: [],
+                        pictures: [],
+                        pricePerHalfHour: 0,
+                        possibleDate: []
+                    )
+                )
+            }
+            .retry(3)
     }
     
     func createRandomNickname() -> Observable<String> {
