48124c9934a625ebdafff3228ab89f720f2ca27d
chansooo <kcs05008@gmail.com>
2022-11-28 00:06:05 +0900

    style: 평점 나타내는 별 들어간 뷰 추가

---
+++ Trinap/Sources/Presenter/Common/Reusable/StarView.swift
@@ -0,0 +1,59 @@
+//
+//  StarView.swift
+//  Trinap
+//
+//  Created by kimchansoo on 2022/11/27.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import UIKit
+
+import Than
+import SnapKit
+
+// 네이밍 추천 받읍니다..
+final class StarView: BaseView {
+    
+    // MARK: UI
+    private lazy var starImage = UIImageView().than {
+        $0.image = TrinapAsset.reviewStar.image
+    }
+    
+    private lazy var ratingLabel = UILabel().than {
+        $0.font = TrinapFontFamily.Pretendard.semiBold.font(size: 14)
+    }
+    
+    // MARK: Properties
+    
+    // MARK: Initializers
+    
+    // MARK: Methods
+    override func configureHierarchy() {
+        self.addSubviews([
+            starImage,
+            ratingLabel
+        ])
+    }
+    
+    override func configureConstraints() {
+        starImage.snp.makeConstraints { make in
+            make.width.height.equalTo(18)
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        ratingLabel.snp.makeConstraints { make in
+            make.leading.equalTo(starImage.snp.trailing).offset(3)
+            make.trailing.centerY.equalToSuperview()
+        }
+    }
+    
+    override func configureAttributes() {}
+    
+    override func bind() {}
+    
+    func configure(rating: Double) {
+        ratingLabel.text = "\(rating)"
+    }
+
+}
