4da859b58c6c1754cd1f7da5575ec34e35014df2
chansooo <kcs05008@gmail.com>
2022-11-28 00:07:43 +0900

    feat: diffableDataSource 적용

--- Trinap/Sources/Presenter/TabBar/Main/PhotographerList/PhotographerListViewController.swift
+++ Trinap/Sources/Presenter/TabBar/Main/PhotographerList/PhotographerListViewController.swift
@@ -15,6 +15,7 @@ import SnapKit
 final class PhotographerListViewController: BaseViewController {
 
     // MARK: - UI
+    //TODO: 디자인이 다르고 입력받을 일이 없어서 uiview로 변경하기
     private lazy var searchBar = UISearchBar().than {
         $0.setImage(UIImage(named: "icSearchNonW"), for: UISearchBar.Icon.search, state: .normal)
         $0.placeholder = "추억을 만들 장소를 선택해주세요."
@@ -22,7 +23,19 @@ final class PhotographerListViewController: BaseViewController {
     
     private lazy var filterView = FilterView(filterMode: .main)
     
-//    private lazy var containerViewController = ContainerView
+    private lazy var layout = UICollectionViewFlowLayout().than {
+        let width = self.view.frame.width - (2 * trinapOffset)
+        let height = width * 0.8
+        $0.minimumLineSpacing = 10
+        $0.estimatedItemSize = CGSize(width: width, height: height)
+        $0.scrollDirection = .vertical
+    }
+        
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout).than {
+        $0.register(PhotographerPreviewCell.self)
+    }
+    
+    private var datasource: UICollectionViewDiffableDataSource<Section, PhotographerPreview>?
     
     // MARK: - Properties
     private let viewModel: PhotographerListViewModel
@@ -43,7 +56,8 @@ final class PhotographerListViewController: BaseViewController {
     override func configureHierarchy() {
         self.view.addSubviews([
             searchBar,
-            filterView
+            filterView,
+            collectionView
         ])
     }
 
@@ -58,10 +72,18 @@ final class PhotographerListViewController: BaseViewController {
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(trinapOffset * 6)
         }
+        
+        collectionView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(trinapOffset)
+            make.trailing.equalToSuperview().offset(-trinapOffset)
+            make.top.equalTo(filterView.snp.bottom).offset(15)
+            make.bottom.equalToSuperview()
+        }
     }
 
     override func configureAttributes() {
         searchBar.isUserInteractionEnabled = false
+        datasource = configureDataSource()
     }
 
     override func bind() {
@@ -75,6 +97,8 @@ final class PhotographerListViewController: BaseViewController {
             .when(.recognized).asObservable()
             .map { _ in return () }
         
+        //TODO: 선택된 셀 인덱스로 넘겨주기
+        
         let input = PhotographerListViewModel.Input(
             searchTrigger: searchTrigger,
             tagType: type
@@ -83,8 +107,40 @@ final class PhotographerListViewController: BaseViewController {
         let output = viewModel.transform(input: input)
         
         output.previews
-            .drive(onNext: { print("drive: \($0)")})
+            .compactMap { [weak self] previews in
+                //TODO: 값이 빈 배열이 넘어오면 실행되지 않음,,,,,,,
+                Logger.print("쨔스")
+                Logger.printArray(previews)
+                return self?.generateSnapshot(sources: previews)
+            }
+            .drive(onNext: { [weak self] snapshot in
+                self?.datasource?.apply(snapshot, animatingDifferences: true)
+            })
             .disposed(by: disposeBag)
-        
     }
+}
+
+extension PhotographerListViewController {
+    
+    enum Section: CaseIterable {
+        case main
+    }
+    
+    func generateSnapshot(sources: [PhotographerPreview]) -> NSDiffableDataSourceSnapshot<Section, PhotographerPreview> {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, PhotographerPreview>()
+        
+        snapshot.appendSections([.main])
+        snapshot.appendItems(sources)
+        return snapshot
+    }
+    
+    func configureDataSource() -> UICollectionViewDiffableDataSource<Section, PhotographerPreview> {
+        return UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueCell(PhotographerPreviewCell.self, for: indexPath)
+            else { return UICollectionViewCell() }
+            cell.configureCell(itemIdentifier)
+            return cell
+        }
+    }
+
 }
