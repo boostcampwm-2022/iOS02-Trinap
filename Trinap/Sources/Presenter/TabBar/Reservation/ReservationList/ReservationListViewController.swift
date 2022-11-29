36d6a9ad3498d0b05febc92d415646e0f198dd54
Seyoung Kim <yy0867@gmail.com>
2022-11-29 11:09:21 +0900

    feat: ReservationListViewController UI 구현

--- Trinap/Sources/Presenter/TabBar/Reservation/ReservationList/ReservationListViewController.swift
+++ Trinap/Sources/Presenter/TabBar/Reservation/ReservationList/ReservationListViewController.swift
@@ -8,17 +8,69 @@
 
 import UIKit
 
+import SnapKit
+
 final class ReservationListViewController: BaseViewController {
     
+    private enum Section {
+        case main
+    }
+    
     // MARK: - UI
+    private lazy var filterView = FilterView(filterMode: .reservation)
     
     // MARK: - Properties
+    private lazy var reservationListTableView = UITableView().than {
+        $0.separatorStyle = .none
+        $0.register(ReservationCell.self)
+    }
+    
+    private var dataSource: UITableViewDiffableDataSource<Section, Reservation>?
     
     // MARK: - Initializers
-
     
     // MARK: - Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+        self.view.addSubviews([filterView, reservationListTableView])
+    }
+    
+    override func configureConstraints() {
+        super.configureConstraints()
+        
+        filterView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+            make.height.equalTo(trinapOffset * 6)
+        }
+        
+        reservationListTableView.snp.makeConstraints { make in
+            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureAttributes() {
+        super.configureAttributes()
+        
+        configureDataSource()
+    }
+}
+
+// MARK: - Privates
+private extension ReservationListViewController {
+    
+    func configureDataSource() {
+        let dataSource = UITableViewDiffableDataSource<Section, Reservation>(
+            tableView: self.reservationListTableView,
+            cellProvider: reservationCellProvider
+        )
+    }
+    
+    func reservationCellProvider(tableView: UITableView, indexPath: IndexPath, item: Reservation) -> UITableViewCell {
+        guard let cell = tableView.dequeueCell(ReservationCell.self, for: indexPath) else {
+            return UITableViewCell()
+        }
+        
+        return cell
     }
 }
