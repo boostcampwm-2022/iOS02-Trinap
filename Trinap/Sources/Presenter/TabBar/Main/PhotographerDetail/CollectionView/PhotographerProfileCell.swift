8195f36849952be1243a5d9cb3919ec7ce24802f
chansooo <kcs05008@gmail.com>
2022-11-30 01:43:06 +0900

    feat: `PhotographerProfileCell` 구현

---
+++ Trinap/Sources/Presenter/TabBar/Main/PhotographerDetail/CollectionView/PhotographerProfileCell.swift
@@ -0,0 +1,67 @@
+//
+//  PhotographerProfileCell.swift
+//  Trinap
+//
+//  Created by kimchansoo on 2022/11/29.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import UIKit
+
+final class PhotographerProfileCell: BaseCollectionViewCell {
+    
+    // MARK: UI
+    lazy var filterView = FilterView(filterMode: .photographer)
+    private lazy var profileImage = ProfileImageView()
+    
+    private lazy var nickNameLabel = UILabel().than {
+        $0.text = "어디로든떠나요"
+        $0.textColor = .black
+        $0.font = TrinapFontFamily.Pretendard.bold.font(size: 20)
+    }
+     
+    private lazy var locationLabel = UILabel().than {
+        $0.text = "서울시 성동구"
+        $0.textColor = TrinapAsset.gray40.color
+        $0.font = TrinapFontFamily.Pretendard.regular.font(size: 14)
+    }
+    
+    // MARK: Properties
+    
+    // MARK: Initializers
+    
+    // MARK: Methods
+    override func configureHierarchy() {
+        self.addSubviews([profileImage, nickNameLabel, locationLabel, filterView])
+    }
+    
+    override func configureConstraints() {
+        profileImage.snp.makeConstraints { make in
+            make.width.height.equalTo(trinapOffset * 8)
+            make.leading.equalToSuperview().inset(trinapOffset * 2)
+            make.top.equalToSuperview().inset(trinapOffset * 3)
+        }
+
+        nickNameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImage.snp.trailing).offset(trinapOffset * 2)
+            make.top.equalTo(profileImage).offset(trinapOffset / 2)
+        }
+
+        locationLabel.snp.makeConstraints { make in
+            make.leading.equalTo(nickNameLabel)
+            make.top.equalTo(nickNameLabel.snp.bottom).offset(trinapOffset / 2)
+        }
+        
+        filterView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(profileImage.snp.bottom).offset(trinapOffset * 2)
+            make.height.equalTo(trinapOffset * 6)
+        }
+    }
+    
+    func configure(with profile: PhotographerUser) {
+        nickNameLabel.text = profile.nickname
+        locationLabel.text = profile.location
+        profileImage.setImage(at: profile.profileImage)
+    }
+}
