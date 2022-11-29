50fc3391c9cde750c7399599afc290c92dec750d
Seyoung Kim <yy0867@gmail.com>
2022-11-29 17:07:36 +0900

    fix: ReservationRepository 수정

--- Trinap/Sources/Domain/Repositories/Reservation/ReservationRepository.swift
+++ Trinap/Sources/Domain/Repositories/Reservation/ReservationRepository.swift
@@ -13,10 +13,10 @@ import RxSwift
 protocol ReservationRepository {
     
     // MARK: Methods
-    func fetchReceivedReservations() -> Observable<[Reservation]>
-    func fetchSentReservations() -> Observable<[Reservation]>
-    func fetchDetail(reservationId: String) -> Observable<Reservation>
-    func addReservation(reservation: Reservation) -> Observable<Bool>
-    func deleteReservation(reservationId: String) -> Observable<Void>
-    func updateState(reservationId: String, state: Reservation.State) -> Observable<Void>
+    func fetchReceivedReservations() -> Observable<[Reservation.Mapper]>
+    func fetchSentReservations() -> Observable<[Reservation.Mapper]>
+//    func fetchDetail(reservationId: String) -> Observable<Reservation>
+//    func addReservation(reservation: Reservation) -> Observable<Bool>
+//    func deleteReservation(reservationId: String) -> Observable<Void>
+//    func updateState(reservationId: String, state: Reservation.Status) -> Observable<Void>
 }
