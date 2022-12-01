fd7270cb10735b608de829de7ee5a874c0e429bd
Seyoung Kim <yy0867@gmail.com>
2022-12-02 02:59:48 +0900

    feat: Reservation Status를 UI 구성용으로 변환할 수 있는 프로토콜 추가

---
+++ Trinap/Sources/Presenter/TabBar/Reservation/Detail/HandleStatus/ReservationStatusConvertible.swift
@@ -0,0 +1,12 @@
+//
+//  ReservationStatusConvertible.swift
+//  Trinap
+//
+//  Created by 김세영 on 2022/12/02.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+protocol ReservationStatusConvertible {
+    
+    func convert() -> ReservationStatusConfigurations
+}
