290bf2cddf5e047438fec488b453248d50d24234
Seyoung Kim <yy0867@gmail.com>
2022-12-01 17:05:29 +0900

    feat: 예약내역 고객 및 작가 뷰 구현

---
+++ Trinap/Sources/Presenter/TabBar/Reservation/Detail/ReservationUserView.swift
@@ -0,0 +1,79 @@
+//
+//  ReservationUserView.swift
+//  Trinap
+//
+//  Created by 김세영 on 2022/12/01.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ReservationUserView: BaseView {
+    
+    enum UserType: String {
+        case photographer = "작가님"
+        case customer = "고객님"
+    }
+    
+    // MARK: - UI
+    private lazy var userTypeLabel = UILabel().than {
+        $0.text = "\(userType.rawValue) 정보"
+        $0.font = TrinapFontFamily.Pretendard.bold.font(size: 16)
+        $0.textColor = TrinapAsset.subtext.color
+    }
+    
+    private lazy var profileImageView = ProfileImageView()
+    
+    private lazy var nicknameLabel = UILabel().than {
+        $0.text = "\(userType.rawValue)"
+        $0.font = TrinapFontFamily.Pretendard.bold.font(size: 16)
+        $0.textColor = TrinapAsset.black.color
+    }
+    
+    // MARK: - Properties
+    private let userType: UserType
+    
+    // MARK: - Initializers
+    init(userType: UserType) {
+        self.userType = userType
+        
+        super.init(frame: .zero)
+    }
+    
+    // MARK: - Methods
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        
+        self.addSubviews([
+            userTypeLabel,
+            profileImageView,
+            nicknameLabel
+        ])
+    }
+    
+    override func configureConstraints() {
+        super.configureConstraints()
+        
+        userTypeLabel.snp.makeConstraints { make in
+            make.leading.top.equalToSuperview()
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(userTypeLabel.snp.bottom).offset(16)
+            make.width.height.equalTo(48)
+            make.leading.bottom.equalToSuperview()
+        }
+        
+        nicknameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
+            make.centerY.equalTo(profileImageView.snp.centerY)
+        }
+    }
+    
+    func setUser(_ user: User) {
+        self.profileImageView.setImage(at: user.profileImage)
+        self.nicknameLabel.text = "\(user.nickname) \(userType.rawValue)"
+    }
+}
