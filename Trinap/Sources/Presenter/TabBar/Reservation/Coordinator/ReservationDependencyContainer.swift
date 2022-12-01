7e16f86c0851d365217e99833a864f8316a611a8
Seyoung Kim <yy0867@gmail.com>
2022-12-01 11:01:58 +0900

    feat: Dependency를 관리하는 컨테이너 생성

---
+++ Trinap/Sources/Presenter/TabBar/Reservation/Coordinator/ReservationDependencyContainer.swift
@@ -0,0 +1,52 @@
+//
+//  ReservationDependencyContainer.swift
+//  Trinap
+//
+//  Created by 김세영 on 2022/12/01.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import FirestoreService
+
+final class ReservationDependencyContainer {
+    
+    // MARK: - Long Lived
+    let firestoreService: FireStoreService
+    let reservationRepository: ReservationRepository
+    let userRepository: UserRepository
+
+    weak var reservationCoordinator: ReservationCoordinator?
+    
+    init(reservationCoordinator: ReservationCoordinator?) {
+        self.firestoreService = DefaultFireStoreService()
+        self.reservationCoordinator = reservationCoordinator
+        
+        self.reservationRepository = DefaultReservationRepository(firebaseStoreService: firestoreService)
+        self.userRepository = DefaultUserRepository(firestoreService: firestoreService)
+    }
+    
+    // MARK: - PreviewList
+    func makeReservationPreviewListViewController() -> ReservationPreviewListViewController {
+        let viewModel = makeReservationPreviewListViewModel()
+        
+        return ReservationPreviewListViewController(viewModel: viewModel)
+    }
+    
+    private func makeReservationPreviewListViewModel() -> ReservationPreviewListViewModel {
+        let fetchReservationPreviewsUseCase = makeFetchReservationPreviewsUseCase()
+        
+        return ReservationPreviewListViewModel(
+            coordinator: reservationCoordinator,
+            fetchReservationPreviewsUseCase: fetchReservationPreviewsUseCase
+        )
+    }
+    
+    private func makeFetchReservationPreviewsUseCase() -> FetchReservationPreviewsUseCase {
+        return DefaultFetchReservationPreviewsUseCase(
+            reservationRepository: reservationRepository,
+            userRepository: userRepository
+        )
+    }
+    
+    // MARK: - Detail
+}
