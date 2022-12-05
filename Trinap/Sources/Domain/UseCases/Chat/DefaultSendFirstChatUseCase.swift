d0c9d3a5ca84ab46aea349296dd0bfed2805e3dc
chansooo <kcs05008@gmail.com>
2022-12-06 00:25:47 +0900

    feat: 첫번째 채팅 보내는 로직 구현

---
+++ Trinap/Sources/Domain/UseCases/Chat/DefaultSendFirstChatUseCase.swift
@@ -0,0 +1,31 @@
+//
+//  DefaultSendFirstChatUseCase.swift
+//  Trinap
+//
+//  Created by kimchansoo on 2022/12/05.
+//  Copyright © 2022 Trinap. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+final class DefaultSendFirstChatUseCase: SendFirstChatUseCase {
+
+    // MARK: Properties
+    private let chatRepository: ChatRepository
+    
+    // MARK: Initializers
+    init(chatRepository: ChatRepository) {
+        self.chatRepository = chatRepository
+    }
+    
+    // MARK: Methods
+    func send(chatroomId: String) -> Observable<Void> {
+        return chatRepository.send(
+            chatType: .reservation,
+            content: "",
+            at: chatroomId
+        )
+    }
+}
