e3e348d1a74d85bdd18d93e7cd2eaabf48d29662
chansooo <kcs05008@gmail.com>
2022-12-28 16:01:28 +0900

    feat: ConfigType으로 MemoryCacheConfig와 DiskCacheConfig 설정할 수 있도록 구현

--- Queenfisher/Sources/ImageCache/ImageCacheConfig.swift
+++ Queenfisher/Sources/ImageCache/ImageCacheConfig.swift
@@ -8,30 +8,43 @@
 
 import Foundation
 
-public struct Config {
+public struct MemoryConfig {
     var totalCostLimit: Int =  150 * 1024 * 1024 // 메모리 캐시 최대 용량.
     var countLimit: Int = 50 // 메모리 캐시 최대 적재 개수.
 }
+
+typealias DiskConfig = Int
 
 public enum ConfigType {
     case lower
     case normal
     case high
     
-    var config: Config {
+    var memoryConfig: MemoryConfig {
         switch self {
         case .lower:
-            return Config(
+            return MemoryConfig(
                 totalCostLimit: 70 * 1024 * 1024,
                 countLimit: 25
             )
         case .normal:
-            return Config()
+            return MemoryConfig()
         case .high:
-            return Config(
+            return MemoryConfig(
                 totalCostLimit: 300 * 1024 * 1024,
                 countLimit: 75
             )
+        }
+    }
+    
+    var diskConfig: DiskConfig {
+        switch self {
+        case .lower:
+            return 30
+        case .normal:
+            return 50
+        case .high:
+            return 70
         }
     }
 }
