28f951f85b003fd8844f2c8023f16e02ef8055c3
chansooo <kcs05008@gmail.com>
2022-12-30 14:31:19 +0900

    fix: 캐시 로직 코드 간결과 및 이미지 캐싱하는 dataTask 전달하도록 변경

--- Queenfisher/Sources/ImageCache/ImageCacheImplements.swift
+++ Queenfisher/Sources/ImageCache/ImageCacheImplements.swift
@@ -13,7 +13,9 @@ public final class DefaultImageCache: ImageCacheProtocol {
     // MARK: - Properties
     private let memoryCache = MemoryCacheStorage()
     private let diskCache = DiskCacheStorage()
+    private let imageDownloader = ImageDownloader()
     
+    private var lastImage: CacheableImage?
 //    여기서 imageData 하나의 크기까지 지정해주려고 했는데 data 자체의 크기는 크지 않고, image로 바꾸는 연산이 들어가야하므로 빼기로 결정했습니다.
 //    private let imageCostLimit: Int
     
@@ -23,20 +25,39 @@ public final class DefaultImageCache: ImageCacheProtocol {
     }
     
     // MARK: - Methods
-    public func fetch(at url: URL, completion: @escaping (CacheableImage?) -> Void) {
-        memoryCache.fetch(at: url) { [weak self] cacheableImage in
-            if let cacheableImage {
-                completion(cacheableImage)
-                return
-            }
-            
-            // disk cache fetch
-            self?.diskCache.fetch(at: url) { [weak self] diskImage in
-                self?.executeDiskCacheLogic(diskImage: diskImage, url: url) { image in
-                    completion(image)
-                }
-            }
+    public func fetchCached(at url: URL) -> CacheableImage? {
+        return lastImage
+    }
+    
+    public func fetch(at url: URL, completion: @escaping (CacheableImage?) -> Void) -> URLSessionDataTask? {
+        
+        if let memoryCached = memoryCache.fetch(at: url) {
+            completion(memoryCached)
+            return nil
         }
+        
+        let diskCached = diskCache.fetch(at: url)
+        var returnImage: CacheableImage?
+        let task = self.executeDiskCacheLogic(diskImage: diskCached, url: url) { [weak self] image in
+            returnImage = image
+            self?.lastImage = image
+        }
+        completion(returnImage)
+        return task
+//        memoryCache.fetch(at: url) { [weak self] cacheableImage in
+//            if let cacheableImage {
+//                completion(cacheableImage)
+//                return
+//            }
+//
+//            // disk cache fetch
+//            self?.diskCache.fetch(at: url) { [weak self] diskImage in
+//                task = self?.executeDiskCacheLogic(diskImage: diskImage, url: url) { image in
+//                    completion(image)
+//                }
+//            }
+//        }
+//        return task
     }
     
     func config(_ configType: ConfigType) {
@@ -51,26 +72,29 @@ public final class DefaultImageCache: ImageCacheProtocol {
 // MARK: 이미지 캐시 로직
 extension DefaultImageCache {
     
-    private func executeDiskCacheLogic(diskImage: CacheableImage?, url: URL, completion: @escaping (CacheableImage?) -> Void) {
+    private func executeDiskCacheLogic(diskImage: CacheableImage?, url: URL, completion: @escaping (CacheableImage?) -> Void) -> URLSessionDataTask {
         // 캐시에 값 O
+        let dataTask: URLSessionDataTask
         if let diskImage {
-            diskCacheHitted(diskImage: diskImage, url: url) { cacheableImage in
+            dataTask = diskCacheHitted(diskImage: diskImage, url: url) { cacheableImage in
                 completion(cacheableImage)
                 return
             }
+            
         }
         // 캐시에 값 X
         else {
-            diskCacheNotHitted(url: url) { cacheableImage in
+            dataTask = diskCacheNotHitted(url: url) { cacheableImage in
                 completion(cacheableImage)
                 return
             }
         }
         completion(nil)
+        return dataTask
     }
     
-    private func diskCacheHitted(diskImage: CacheableImage, url: URL, completion: @escaping (CacheableImage?) -> Void) {
-        self.fetchImage(at: url, etag: diskImage.etag) { [weak self] result in
+    private func diskCacheHitted(diskImage: CacheableImage, url: URL, completion: @escaping (CacheableImage?) -> Void) -> URLSessionDataTask {
+        let dataTask = self.imageDownloader.fetchImage(at: url, etag: diskImage.etag) { [weak self] result in
             switch result {
             case .success(let networkImage): // 데이터 변경되어서 새로운 데이터 받아왔을 경우
                 self?.diskCache.save(of: networkImage, at: url)
@@ -88,10 +112,11 @@ extension DefaultImageCache {
                 }
             }
         }
+        return dataTask
     }
     
-    private func diskCacheNotHitted(url: URL, completion: @escaping (CacheableImage?) -> Void) {
-        self.fetchImage(at: url, etag: nil) { [weak self] result in
+    private func diskCacheNotHitted(url: URL, completion: @escaping (CacheableImage?) -> Void) -> URLSessionDataTask {
+        let dataTask = self.imageDownloader.fetchImage(at: url, etag: nil) { [weak self] result in
             switch result {
             case .success(let cacheableImage):
                 self?.memoryCache.save(at: url, of: cacheableImage)
@@ -103,5 +128,6 @@ extension DefaultImageCache {
                 return
             }
         }
+        return dataTask
     }
 }
