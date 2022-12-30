f102b06417bb70defa00582f92c54d2a2210ad44
chansooo <kcs05008@gmail.com>
2022-12-30 14:29:15 +0900

    fix: 반복 사용되는 부분 함수로 빼기, dataTask resume하는 위치 변경

--- Queenfisher/Sources/View/ImageView+QF.swift
+++ Queenfisher/Sources/View/ImageView+QF.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
+private var imageTaskKey: Void?
+
 extension QueenfisherWrapper where Base: QFImageView {
+    // MARK: UI
     
+    // MARK: Properties
+    private var imageTask: URLSessionDataTask? {
+        get { return getAssociatedObject(base, &imageTaskKey) }
+        set { setRetainedAssociatedObject(base, &imageTaskKey, newValue)}
+    }
+    
+    // MARK: Methods
     public func setImage(
         at url: URL?,
         placeholder: QFImage? = nil,
@@ -24,35 +34,67 @@ extension QueenfisherWrapper where Base: QFImageView {
             base.image = placeholder
             return
         }
-        
+        var mutableSelf = self
         startIndicator(indicator)
         
         let maybeCache = imageCache(performance: performance)
-
-        maybeCache.fetch(at: url) { cacheableImage in
+        // 여기서 dataTask가 nil이 아니다 -> dataTask.resume하면 됨
+        // dataTask가 nil이다 -> 그냥 받아온 cacheableImage 가지고 와서 처리해주면 됨
+        let dataTask = maybeCache.fetch(at: url) { cacheableImage in
             DispatchQueue.main.async {
-                defer { self.stopIndicator(indicator) }
-                completion?(.zero)
-                
-                guard let cacheableImage else {
-                    base.image = placeholder
-                    completion?(base.image?.size ?? .zero)
-                    return
-                }
-                let data = cacheableImage.imageData
-                
-                if downsampling {
-                    self.base.image = data.imageWithDownsampling(
-                        to: targetSize ?? self.base.frame.size,
-                        scale: scale
-                    )
-                } else {
-                    self.base.image = UIImage(data: data)
-                }
-                
-                completion?(self.base.image?.size ?? .zero)
+                setImageLogic(
+                    cacheableImage: cacheableImage,
+                    indicator: indicator,
+                    placeholder: placeholder,
+                    downsampling: downsampling,
+                    targetSize: targetSize,
+                    scale: scale) { size in
+                        completion?(size)
+                    }
             }
         }
+        guard let dataTask else {
+            let image = maybeCache.fetchCached(at: url)
+            setImageLogic(
+                cacheableImage: image,
+                indicator: indicator,
+                placeholder: placeholder,
+                downsampling: downsampling,
+                targetSize: targetSize,
+                scale: scale) { size in
+                    completion?(size)
+                }
+            return
+        }
+        dataTask.resume()
+        mutableSelf.imageTask = dataTask
+    }
+    
+    public func cancelDownloadTask() {
+        imageTask?.cancel()
+    }
+    
+    private func setImageLogic(cacheableImage: CacheableImage?, indicator: QFIndicator?,  placeholder: QFImage?, downsampling: Bool, targetSize: CGSize?, scale: CGFloat, completion: ((CGSize) -> Void)?) {
+        defer { self.stopIndicator(indicator) }
+        completion?(.zero)
+        
+        guard let cacheableImage else {
+            base.image = placeholder
+            completion?(base.image?.size ?? .zero)
+            return
+        }
+        let data = cacheableImage.imageData
+        
+        if downsampling {
+            self.base.image = data.imageWithDownsampling(
+                to: targetSize ?? self.base.frame.size,
+                scale: scale
+            )
+        } else {
+            self.base.image = UIImage(data: data)
+        }
+        
+        completion?(self.base.image?.size ?? .zero)
     }
     
     private func startIndicator(_ indicator: QFIndicator?) {
@@ -82,9 +124,25 @@ extension QueenfisherWrapper where Base: QFImageView {
     }
 }
 
+private extension QueenfisherWrapper {
+    func getAssociatedObject<T>(_ object: Any, _ key: UnsafeRawPointer) -> T? {
+        return objc_getAssociatedObject(object, key) as? T
+    }
+
+    func setRetainedAssociatedObject<T>(_ object: Any, _ key: UnsafeRawPointer, _ value: T) {
+        objc_setAssociatedObject(object, key, value, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+}
+
+
 private final class NoImageCache: ImageCacheProtocol {
-    func fetch(at url: URL, completion: @escaping (CacheableImage?) -> Void) {
-        self.fetchImage(at: url, etag: nil) { result in
+    
+    // MARK: Properties
+    private let imageDownloader = ImageDownloader()
+    
+    // MARK: Methods
+    func fetch(at url: URL, completion: @escaping (CacheableImage?) -> Void) -> URLSessionDataTask? {
+        let task = self.imageDownloader.fetchImage(at: url, etag: nil) { result in
             switch result {
             case .success(let success):
                 completion(success)
@@ -94,5 +152,11 @@ private final class NoImageCache: ImageCacheProtocol {
                 return
             }
         }
+        
+        return task
+    }
+    
+    func fetchCached(at url: URL) -> CacheableImage? {
+        return nil
     }
 }
